980d713bce5a70715f071b34239cd7c71937c26d
yannikbleistein <yannikbleistein@gmx.de>
2023-09-25 11:23:37 +0200

    am bearbeiten

---
+++ app/src/main/java/com/example/paymessage/Adapter/FavoriteAdapter.kt
@@ -0,0 +1,50 @@
+package com.example.paymessage.Adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.example.paymessage.R
+import com.example.paymessage.data.datamodels.News
+import com.example.paymessage.databinding.FavoriteItemBinding
+import com.example.paymessage.ui.NewsViewModel
+
+class FavoriteAdapter (
+
+    private val viewModel: NewsViewModel,
+    private var dataset: List<News>
+): RecyclerView.Adapter<FavoriteAdapter.ViewHolder>(){
+
+    inner class ViewHolder(val binding: FavoriteItemBinding) :
+        RecyclerView.ViewHolder(binding.root)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FavoriteAdapter.ViewHolder {
+        val binding =
+            FavoriteItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = dataset[position]
+        holder.binding.favoriteTV.text = item.title
+        holder.binding.favoriteIV.load(item.teaserImage.imageVariants.image144)
+
+
+        val likeImageResource = if (item.isLiked) R.drawable.baseline_star_24
+        else R.drawable.baseline_star_outline_24
+        holder.binding.favoriteStarBTN.setImageResource(likeImageResource)
+
+        holder.binding.favoriteStarBTN.setOnClickListener {
+            val like = dataset[position]
+            like.isLiked = !like.isLiked
+            notifyItemChanged(position)
+
+            //Datenbank updaten
+            viewModel.updateLikestatusInDb(like)
+        }
+    }
+
+
+        override fun getItemCount(): Int {
+            return dataset.size
+        }
+    }
--- app/src/main/java/com/example/paymessage/MainActivity.kt
+++ app/src/main/java/com/example/paymessage/MainActivity.kt
@@ -3,14 +3,18 @@ package com.example.paymessage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.example.paymessage.databinding.ActivityMainBinding
 import com.example.paymessage.ui.ArtikelFragmentDirections
+import com.example.paymessage.ui.FavoriteFragmentDirections
 import com.example.paymessage.ui.HomeFragmentDirections
 import com.example.paymessage.ui.SettingsFragmentDirections
+import java.lang.Exception
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
@@ -26,59 +30,67 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         navController = navHostFragment.navController
-
-        binding.bottomNavigationView.setOnItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.settings -> {
-                    //  val navController = findNavController(R.id.fragmentContainerView)
-
-                    val currentDestination = navController.currentDestination
-                    if (currentDestination?.id == R.id.artikelFragment) {
-                        val action = HomeFragmentDirections.actionHomeFragmentToSettingsFragment()
-                        navController.navigate(action)
-                    } else if (currentDestination?.id == R.id.homeFragment) {
-                        val action = HomeFragmentDirections.actionHomeFragmentToSettingsFragment()
-                        navController.navigate(action)
+        try {
+            binding.bottomNavigationView.setOnItemSelectedListener { item ->
+                when (item.itemId) {
+                    R.id.favorite -> {
+                        val currentDestination = navController.currentDestination
+                        if (currentDestination?.id == R.id.artikelFragment) {
+                            val action =
+                                HomeFragmentDirections.actionHomeFragmentToFavoriteFragment()
+                            navController.navigate(action)
+                        } else if (currentDestination?.id == R.id.homeFragment) {
+                            val action =
+                                HomeFragmentDirections.actionHomeFragmentToFavoriteFragment()
+                            navController.navigate(action)
+                        }
+                        true
                     }
-                    true
-                }
 
-                R.id.settings -> {
-                    val currentDestination = navController.currentDestination
-                    if (currentDestination?.id == R.id.artikelFragment) {
-                        // Wenn das aktuelle Ziel das Detailfragment ist, navigiere zu den Einstellungen
-                        val action = ArtikelFragmentDirections.actionArtikelFragmentToSettingsFragment()
-                        navController.navigate(action)
-                    } else {
-                        // Wenn das aktuelle Ziel nicht das Detailfragment ist, navigiere zum Homefragment
-                        val action = ArtikelFragmentDirections.actionArtikelFragmentToSettingsFragment()
-                        navController.navigate(action)
+
+                    R.id.settings -> {
+
+                        val currentDestination = navController.currentDestination
+
+                        if (currentDestination?.id == R.id.artikelFragment) {
+                            val action =
+                                HomeFragmentDirections.actionHomeFragmentToSettingsFragment()
+                            navController.navigate(action)
+                        } else if (currentDestination?.id == R.id.homeFragment) {
+                            val action =
+                                HomeFragmentDirections.actionHomeFragmentToSettingsFragment()
+                            navController.navigate(action)
+                        }
+                        true
                     }
-                    true
-                }
 
-                R.id.home -> {
-                    val navController = findNavController(R.id.fragmentContainerView)
-                    val currentDestination = navController.currentDestination
 
-                    if (currentDestination?.id == R.id.artikelFragment) {
-                        val action = ArtikelFragmentDirections.actionArtikelFragmentToHomeFragment()
-                    } else if (currentDestination?.id == R.id.settingsFragment) {
-                        val action =
-                            SettingsFragmentDirections.actionSettingsFragmentToHomeFragment()
-                        navController.navigate(action)
+                    R.id.home -> {
+                        val currentDestination = navController.currentDestination
+                        if (currentDestination?.id == R.id.artikelFragment) {
+                            val action =
+                                ArtikelFragmentDirections.actionArtikelFragmentToHomeFragment()
+                            navController.navigate(action)
+                        } else if (currentDestination?.id == R.id.settingsFragment) {
+                            val action =
+                                SettingsFragmentDirections.actionSettingsFragmentToHomeFragment()
+                            navController.navigate(action)
+                        }
+                        true
                     }
-                    true
-                }
 
 
-
-                else -> {// Standard-Navigation zu anderen Fragmenten
-                    NavigationUI.onNavDestinationSelected(item, navController!!)
-                    navController?.popBackStack(item.itemId, false)
-                    true
+                    else -> {// Standard-Navigation zu anderen Fragmenten
+                        NavigationUI.onNavDestinationSelected(item, navController!!)
+                        navController?.popBackStack(item.itemId, false)
+                        true
+                    }
                 }
             }
+        } catch (
+            e: Exception
+        ) {
+            Log.e("Settings", "${e}")
         }
     }
 }

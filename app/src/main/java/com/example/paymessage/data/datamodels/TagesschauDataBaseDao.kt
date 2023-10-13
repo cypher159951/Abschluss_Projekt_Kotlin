a843690409305bbc00587b94ea00c5db643c2b2c
yannikbleistein <yannikbleistein@gmx.de>
2023-10-13 11:59:20 +0200

    ältere daten aus datenbank löschen

--- app/src/main/java/com/example/paymessage/data/datamodels/TagesschauDataBaseDao.kt
+++ app/src/main/java/com/example/paymessage/data/datamodels/TagesschauDataBaseDao.kt
@@ -9,31 +9,43 @@ import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
 
+
+// Definiert eine DAO-Schnittstelle für den Zugriff auf die Tagesschau-Datenbank.
 @Dao
 interface TagesschauDataBaseDao {
 
+    // Fügt Nachrichten in die Datenbank ein. Bei Konflikt wird das bestehende Element ersetzt.
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertall(news: News)
 
+
+    // Ermittelt die Anzahl der Datensätze in der 'tagesschau'-Tabelle
     @Query("SELECT COUNT(*) FROM tagesschau ")
     fun checkDataCount(): Int
 
+
+    // Aktualisiert ein bestimmtes Element in der Datenbank.
     @Update
     fun updateItem(news: News)
 
 
+    // Ruft ein Element anhand seiner 'sophoraId' aus der Datenbank ab.
     @Query("SELECT * FROM tagesschau WHERE sophoraId = :itemId")
     fun getItemById(itemId: String): News
 
 
+    // Ruft alle als 'liked' markierten Elemente aus der Datenbank ab.
     @Query("SELECT * FROM tagesschau WHERE isLiked = 1")
     fun getLiked(): LiveData<List<News>>
 
 
-    //ASC für aufsteigend
+    // ASC für aufsteigend
+    // Ruft alle Elemente aus der Datenbank in absteigender Reihenfolge nach 'date' ab.
     @Query("SELECT * FROM tagesschau ORDER by date DESC")
     fun getAllItems(): LiveData<List<News>>
 
+
+    // Löscht Elemente aus der Datenbank, die älter als das angegebene Datum sind.
     @Query("DELETE FROM tagesschau WHERE date < :date")
     fun deleteOldItems(date: String)
 
